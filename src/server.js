ba1dbe27ae69114c1d0188f92ca80d3dc20d040e
rrodffer <rferdev@gmail.com>
2023-01-10 23:02:54 -0300

    Feat: add config inicial nodejs

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import http from 'node:http';
+
+/* 
+- CommonJS => require "type": "commonjs"
+- ESModules => import "type": "module"
+* adicionar "type" no package.json
+*/
+
+const server = http.createServer((req, res) => {
+  return res.end('Hello World');
+});
+
+server.listen(3333);
