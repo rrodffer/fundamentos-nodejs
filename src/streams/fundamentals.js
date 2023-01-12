e1431801f3af87b2c26967dea1139063ba7a4e5f
rrodffer <rferdev@gmail.com>
2023-01-12 20:10:33 -0300

    Feat: add strem de escrita e transformacao

--- src/streams/fundamentals.js
+++ src/streams/fundamentals.js
@@ -1,7 +1,8 @@
-import { Readable } from 'node:stream';
+import { Readable, Writable, Transform } from 'node:stream';
 
 // process.stdin.pipe(process.stdout)
 
+//Stream de leitura
 class OneToHundredStream extends Readable {
   index = 1
   _read() {
@@ -18,4 +19,24 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+// Stream que transforma
+class InverseNumber extends Transform {
+  _transform(chunk, encoding, callback) {
+    const number = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(number)));
+  }
+}
+
+// Stream de escrita
+class MultiplyByStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+
+    callback();
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumber())
+  .pipe(new MultiplyByStream());
