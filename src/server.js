a419eea2f90628ecf8ea18590dd484d61328e6c6
rrodffer <rferdev@gmail.com>
2023-01-11 23:44:15 -0300

    Feat: add node watch

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import http from 'node:http';
 */
 
 const server = http.createServer((req, res) => {
-  return res.end('Hello World');
+  return res.end('Hello World! 🍺');
 });
 
 server.listen(3333);
